7c6dd3b87beef49e5efd420757cd3f9716ab1a43
zuzu <dlwnghks6821@naver.com>
2021-12-01 01:51:48 +0900

    페이지 파라미터가 없는경우 예외처리 추가

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,38 @@ const pageCalcul = require('../pageCalcul/pageCalcul'); // 페이징 계산 모�
 /* GET home page. */
 
 router.get('/testApi', function(req, res, next) {
-  
+    let page ;
+    let pageSize ;
     const pageInfo = req.query; // get parameters (queryString)
-    let page = parseInt(pageInfo.page); // nowPage 
-    let pageSize = parseInt(pageInfo.pageSize); // perPage (pageSize)
+    try{
+      
+       page = parseInt(pageInfo.page); // nowPage 
+       pageSize = parseInt(pageInfo.pageSize); // perPage (pageSize)
 
-  
-
+    }catch(error){
+      
+    }
+    
+    if(isNaN(page)){
+      page = 1;
+    }
+    if(isNaN(pageSize)){
+      pageSize = 10;
+    }
+    
     mysqlConn.conn().connect();
     mysqlConn.conn().query('select count(*) as count from temp',(error, data, fields) => {
       let start = 0 ;
       if (error) throw error; 
-  
       if(page <= 0){
         page = 1;
-        }else{
+      }else{
           start = (page -1) * pageSize;
-        }
-        if(page > Math.round(data[0].count) / pageSize){
+      }
+      if(page > Math.round(data[0].count) / pageSize){
           console.log('마지막 페이지입니다.');
             return null;
-        }
+      }
       
 
       mysqlConn.conn().query(`SELECT * FROM temp LIMIT ${start}, ${pageSize}`,(error, data, fields) => {
